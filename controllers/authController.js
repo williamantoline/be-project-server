6e87883b646926ad39324c55db5691203832c863
Angeline Ho <angelineho16.03@gmail.com>
2023-01-17 15:45:51 +0700

    Add JWT in login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,42 @@
+const conn = require("../db");
+
+const getUser = async (username) => {
+  conn.query(`SELECT * FROM users WHERE username='${username}'`, (err, result) => {
+    if(err) return false
+    return result
+  })
+}
+
 const register = (req, res) => {
   res.send('register');
 }
 
-const login = (req, res) => {
-  res.send("login")
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await getUser(username);
+
+  if(!user){
+    return res.status(403).json({
+      message: 'Username not Found',
+      error: "invalid login",
+    })
+  }
+
+  if(user.password !== password){
+    return res.status(403).json({
+      message: 'Wrong Password',
+      error: "invalid login",
+    });
+  }
+
+  const token = jwt.sign(user, process.env.MY_SECRET, { expiresIn: "1h" });
+
+  res.cookie("token", token, {
+    httpOnly: true,
+  })
+
+  return res.redirect("/home");
+
 }
 
 const revoke = (req, res) => {
