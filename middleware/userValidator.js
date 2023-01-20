642aefae1dc8f1f0e76c4dbbc4d37b58519a8343
Angeline Ho <angelineho16.03@gmail.com>
2023-01-20 14:33:50 +0700

    add validation user and insert data in register routes

--- middleware/userValidator.js
+++ middleware/userValidator.js
@@ -1,10 +1,10 @@
 const { body, validationResult } = require('express-validator')
 
 exports.validateUser = [
-  body('name').isAlpha().isLength({ min: 3, max: 255 }),
-  body('username').isLength({ min: 3, max: 255 }),
-  body('email').isEmail(),
-  body('password').isLength({ min: 8, max: 255 }),
+  body('name').notEmpty().isAlpha().isLength({ min: 3, max: 255 }),
+  body('username').notEmpty().isLength({ min: 3, max: 255 }),
+  body('email').notEmpty().isEmail(),
+  body('password').notEmpty().isLength({ min: 8, max: 255 }),
   (req, res, next) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
